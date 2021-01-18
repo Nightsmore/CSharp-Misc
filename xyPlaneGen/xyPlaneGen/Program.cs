842eaf9aae88595ec69ed69c8fdf8ccb5b7ee087
Chris <38567588+Nightsmore@users.noreply.github.com>
2021-01-18 09:41:41 -0800

    Add files via upload

---
+++ xyPlaneGen/xyPlaneGen/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace xyPlaneGen
+{
+    class Program
+    {
+        static int chunkSize = 50;
+        public static int terrainSizeX, terrainSizeZ;
+
+
+        static int[,] mapArray;
+        static void Main(string[] args)
+        {
+            mapArray = new int[10, 10];
+            terrainSizeX = 10;
+            terrainSizeZ = 10;
+
+            for (int i = 0; i != terrainSizeX; ++i)
+            {
+              
+                try
+                {
+                    mapArray[i, 0] = mapArray[i - 1, 0] + chunkSize;
+                }
+                catch (System.IndexOutOfRangeException)
+                {
+
+                }
+
+                for (int ii = 0; ii != terrainSizeZ; ++ii)
+                {
+                    try
+                    {
+                        mapArray[i, ii] = (mapArray[i, ii - 1] + chunkSize);
+                    }
+                    catch (System.IndexOutOfRangeException)
+                    {
+
+                    }
+                }
+            }
+ 
+
+            for (int i = 0; i != terrainSizeX; ++i)
+            {
+
+                for (int ii = 0; ii != terrainSizeZ; ++ii)
+                {
+                    Console.Write("{0},", mapArray[i, ii]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ xyPlaneGen/xyPlaneGen/Vector3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace perlin
+{
+    class Vector3
+    {
+        private float x, y, z;
+        
+        Vector3(float x, float y,float z)
+        {
+            this.x = x;
+            this.y = y; 
+            this.z = z;
+        }
+    }
+}
